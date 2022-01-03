d3d94850559750f4ea88f9f3bc4986084d022bb2
chey <ragha_mrc@outlook.com>
2022-01-03 20:08:37 +0530

    Adding demo git class file

---
+++ src/main/java/com/java/adavanced/gitDemo.java
@@ -0,0 +1,11 @@
+package com.java.adavanced;
+
+public class gitDemo {
+    public static void main(String[] args) {
+
+        String a = "Git Main repo";
+
+        System.out.println(a);
+    }
+
+}
